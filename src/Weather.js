c9be20603b7df639d9c6097c87625012f0e2d121
aishacmela <aishacmela@gmail.com>
2024-04-02 01:45:40 +0200

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -5,29 +5,31 @@ export default function Weather(){
     return <div className="weather">
         
         <form>
-            <input type="search" placeholder="Enter city" className="search-form"/>
-            <input type="submit" value="search" className="submit-form"/>
+            <input type="search" placeholder="Enter city" autoFocus="on" className="search-form col-8 shadow "/>
+            <input type="submit" value="search" className="submit-form col-3"/>
         </form>
         
-        <div className="row">
-            <div className="col-sm-4">
-                <h4>Pretoria</h4>
-                <div>
+        <div className="row mt-5">
+            <div className="col-sm-4 text-center">
+                <h2>Pretoria</h2>
+                <p>
                     Wednesday
-                </div>
+                </p>
                 <h1>12:45</h1>
                 <p>Sunny</p>
             
             </div>
-            <div className="col-sm-4">
+            <div className="col-sm-4 text-center">
                 < img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny"></img>
             </div>
-            <div className="col-sm-4">
+            <div className="col-sm-4 text-center">
                 <ul>
-                    <li>Rain: 10% </li>
-                    <li>Humidity: 80% </li>
-                    <li>Wind: 4 km/hr </li>
+                    <li>Rain: <strong>10%</strong> </li>
+                    <li>Humidity: <strong>80%</strong> </li>
+                    <li>Wind: <strong>4 km/hr</strong> </li>
                 </ul>
+                <span className="temperature">20</span>
+                <span className="unit">°C|F</span>
 
             </div>
 
