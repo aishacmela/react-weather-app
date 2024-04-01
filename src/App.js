a7aaadfee0990451ae2e9ec15dac523144453e26
aishacmela <aishacmela@gmail.com>
2024-04-01 23:14:41 +0200

    added repository link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       React Weather App
+
+      <footer>
+        this page was coded by aisha and it is <a href='https://github.com/aishacmela/react-weather-app' target='_blank'>open-sourced</a> on Github
+      </footer>
     </div>
   );
 }
