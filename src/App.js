2275a7bdfb6df5908bf178b0fc2942df3a13cd6b
aishacmela <aishacmela@gmail.com>
2024-04-01 23:26:57 +0200

    added bootsrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      React Weather App
+      <div className='container'>
+      <h1>Weather App</h1>
 
       <footer>
         this page was coded by aisha and it is <a href='https://github.com/aishacmela/react-weather-app' target='_blank'>open-sourced</a> on Github
       </footer>
+      </div>
     </div>
   );
 }
